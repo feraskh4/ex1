7abc60b762e20ff52e5434f5962b042ffab3dc71
feraskh4 <ferask@campus.technion.ac.il>
2022-04-18 18:35:16 +0300

    fixed all the functions

--- tests/RLEList.c
+++ tests/RLEList.c
@@ -10,7 +10,6 @@ struct RLEList_t{
 };
 
 //implement the functions here
-//majd
 RLEList RLEListCreate() {
     RLEList ptr = malloc(sizeof(*ptr));
     if (!ptr) {
@@ -21,7 +20,6 @@ RLEList RLEListCreate() {
     return ptr;
 }
 
-// feras
 void RLEListDestroy(RLEList list) {
     RLEList ptr, store;
     ptr = list;
@@ -32,13 +30,21 @@ void RLEListDestroy(RLEList list) {
     }
 }
 
-//majd
 RLEListResult RLEListAppend(RLEList list, char value) {
     if (!list || !value) {
-           return RLE_LIST_NULL_ARGUMENT;
+        return RLE_LIST_NULL_ARGUMENT;
     }
-    if (list->value == 0) {
-        list->value = value;
+    RLEList ptr = list;
+    while (ptr->next) {
+        ptr = ptr->next;
+    }
+    if (ptr->amount==0) {
+        ptr->value = value;
+        ptr->amount = 1;
+        return RLE_LIST_SUCCESS;
+    }
+    if (ptr->value==value) {
+        ptr->amount += 1;
         return RLE_LIST_SUCCESS;
     }
     RLEList newHead = RLEListCreate(newHead);
@@ -46,15 +52,11 @@ RLEListResult RLEListAppend(RLEList list, char value) {
            return RLE_LIST_OUT_OF_MEMORY;
     }
     newHead->value = value;
-    RLEList ptr = list;
-    while (ptr->next) {
-        ptr = ptr->next;
-    }
+    newHead->amount = 1;
     ptr->next = newHead;
     return RLE_LIST_SUCCESS;
 }
 
-// feras
 int RLEListSize(RLEList list) {
     if (!list) {
         return -1;
@@ -67,40 +69,47 @@ int RLEListSize(RLEList list) {
     return count;
 }
 
-//majd
 RLEListResult RLEListRemove(RLEList list, int index) {
     if (!list) {
            return RLE_LIST_NULL_ARGUMENT;
     }
-    if (RLEListSize(list) <= index || index < 0) {
+    if (RLEListSize(list)<index||RLEListSize(list)==0) {
             return RLE_LIST_INDEX_OUT_OF_BOUNDS;
     }
     RLEList ptr = list, ptrNext;
-    for (int i = 0; i < index-1; i++) {
+    //if count is not positive that means the char is in this node
+    int count=index;
+    count -= ptr->amount;
+    while (count>0) {
         ptr = ptr->next;
+        count -= ptr->amount;
     }
     if (index==0) {
-        if (ptr->next) {
-            ptr = ptr->next;
-            RLEListDestroy(ptr);
-                return RLE_LIST_SUCCESS;
-        }
-        ptr = NULL;
-        RLEListDestroy(ptr);
+        if (list->next) {
+            if (list->amount==1)
+                 list = list->next;
+            else list->amount -= 1;
             return RLE_LIST_SUCCESS;
-    }
-    ptrNext = ptr->next;
-    if (ptrNext->next) {
-        ptr->next = ptrNext->next;
-        RLEListDestroy(ptrNext);
+        }
+        if(list->amount==1)
+            list = NULL;
+        else list->amount -= 1;
         return RLE_LIST_SUCCESS;
     }
+    //ptrNext is the pointer to the index we want to remove
+    ptrNext = ptr->next;
+    if (ptrNext->next) {
+        if (ptrNext->amount==1)
+             ptr->next = ptrNext->next;
+        else ptrNext->amount -= 1;
+        return RLE_LIST_SUCCESS;
+    }
+    if (ptrNext->amount==1)
     ptr->next = NULL;
-    RLEListDestroy(ptrNext);
+    else ptrNext->amount -= 1;
     return RLE_LIST_SUCCESS;
 }
 
-// feras
 char RLEListGet(RLEList list, int index, RLEListResult *result) {
     if (!list) {
         if (result) {
@@ -126,19 +135,25 @@ char RLEListGet(RLEList list, int index, RLEListResult *result) {
     return ptr->value;
 }
 
-// majd
 RLEListResult RLEListMap(RLEList list, MapFunction map_function) {
-        if (!list) {
-          return  RLE_LIST_NULL_ARGUMENT;
+    if (!list) {
+        return  RLE_LIST_NULL_ARGUMENT;
+    }
+    RLEList new_list = RLEListCreate(), ptr = list;
+
+    char current_char;
+    while (ptr) {
+        for (int i = 1; i <= ptr->amount; i++) {
+            current_char = ptr->value;
+            RLEListAppend(new_list, map_function(current_char));
         }
-        RLEList ptr = list;
-        while (ptr) {
-            ptr->value = map_function(ptr->value);
-        }
-        return RLE_LIST_SUCCESS;
+        ptr = ptr->next;
+    }
+    RLEListDestroy(list);
+    list = new_list;
+    return RLE_LIST_SUCCESS;
 }
 
-//feras
 char* RLEListExportToString(RLEList list, RLEListResult* result) {
     if (!list) {
         if (result) {
